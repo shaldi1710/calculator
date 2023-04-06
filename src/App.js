ad00fd9d91a116ebea340df3e0c83bd41f8cf163
shaldi1710 <faishalhadi.hf@gmail.com>
2023-04-06 11:37:58 +0700

    calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import * as math from "mathjs";
 
-function App() {
+import './App.css';
+import Button from "./Components/Button";
+import Input from "./Components/Input";
+
+
+
+
+const App = () => {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+
+  const addToText = (val) => {
+    setText((text) => [...text, val + " "]);
+  };
+
+  const calculateResult = () => {
+    const input = text.join("");
+
+    setResult(math.evaluate(input));
+  };
+
+  const resetInput = () => {
+    setText("");
+    setResult("");
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='calcu-wrapper'>
+        <Input text={text} result={result} />
+        <div className='row'>
+          <div className='plus'><Button symbol="+" handleClick={addToText} /></div>
+          <div className='minus'><Button symbol="-" handleClick={addToText} /></div>
+          <div className='kali'><Button symbol="*" handleClick={addToText} /></div>
+          <div className='bagi'><Button symbol="/" handleClick={addToText} /></div>
+
+          <div className='lima'><Button symbol="7"  handleClick={addToText} /></div>
+          <div className='enam'><Button symbol="8"  handleClick={addToText} /></div>
+          <div className='tujuh'><Button symbol="9"  handleClick={addToText} /> </div>
+          <div className='delapan'><Button symbol="=" color="coral" handleClick={calculateResult} /></div>
+          
+          <div className='sembilan'><Button symbol="4"  handleClick={addToText} /></div>
+          <div className='sepuluh'><Button symbol="5"  handleClick={addToText} /></div>
+          <div className='sebelas'><Button symbol="6"  handleClick={addToText} /></div>
+          <div className='duabelas'><Button symbol="1"  handleClick={addToText} /></div>
+
+
+          <div className='tigabelas'><Button symbol="2"  handleClick={addToText} /></div>
+          <div className='empatbelas'><Button symbol="3"  handleClick={addToText} /></div>
+          <div className='limabelas'><Button symbol="0"  handleClick={addToText} /></div>
+          <div className='enambelas'><Button symbol="."  handleClick={addToText} /></div>
+
+          <div className='tujuhbelas'><Button symbol="AC"  handleClick={resetInput} /></div>
+          {/* <div classname='row18'><Button symbol="" color="white" handleClick={addToText} /></div>
+          <div className='row19'><Button symbol="AC" color="white" handleClick={addToText} /></div>
+          <div className='row20'><Button symbol="" color="white" handleClick={addToText} /></div> */}
+
+
+
+        </div>
+      </div>
     </div>
   );
 }
